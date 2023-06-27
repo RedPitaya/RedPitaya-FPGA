b5ad45678afffdb68a5b39c88107e26d84d5e942
n.danilyuk <n.danilyuk@inetegrasources.com>
2023-06-27 11:49:28 +0700

    Change path to stream_app_4ch project

--- red_pitaya_vivado_Z20_4.tcl
+++ red_pitaya_vivado_Z20_4.tcl
@@ -59,7 +59,7 @@ set_property verilog_define $prj_defs [current_fileset]
 # file was created from GUI using "write_bd_tcl -force ip/systemZ20.tcl"
 # create PS BD
 set ::gpio_width 33
-if {$prj_name == "stream_app"} {
+if {$prj_name == "stream_app_4ch"} {
 source                            $path_ip/systemZ20_4.tcl
 } else {
 source                            $path_ip/systemZ20_14.tcl
